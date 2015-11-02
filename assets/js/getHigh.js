c72c1aaec56f53ddf16658e85232f0248dfe93ed
Arthur Glomb <arthur@Arthurs-MacBook.fritz.box>
2015-11-02 15:25:47 +0100

    Initial commit

---
+++ assets/js/getHigh.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+	var elem = document.getElementById("unityST").getAttribute('height');
+	console.log(elem);
+
+	var elem2   = document.getElementById("test").style;
+	console.log(elem2);
+}
